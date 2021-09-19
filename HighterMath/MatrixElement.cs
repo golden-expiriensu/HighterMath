a178a90a65b02d76a8e70a73a99c35729e37a9c0
expiriensu <gundari_dan@bk.ru>
2021-09-19 19:45:53 +0700

    add bool value to matrix element

--- HighterMath/MatrixElement.cs
+++ HighterMath/MatrixElement.cs
@@ -2,24 +2,35 @@
 {
     public class MatrixElement
     {
+        readonly bool _isBool = false;
+        readonly bool _boolValue;
         readonly int _intValue;
         readonly float _floatValue;
         readonly double _doubleValue;
-        public double Value => _doubleValue + _floatValue + _intValue; // Since only one value is assigned
-                                                                       // with the sum, we get it
+        public double Value => _doubleValue + _floatValue + _intValue + (_boolValue ? 1 : 0); // Since only one value is assigned
+                                                                                              // with the sum, we get it
 
+        public MatrixElement(bool boolValue)
+        {
+            _boolValue = boolValue;
+            _isBool = true;
+        }
         public MatrixElement(int intValue) => _intValue = intValue;
         public MatrixElement(float floatValue) => _floatValue = floatValue;
         public MatrixElement(double doubleValue) => _doubleValue = doubleValue;
 
         public static MatrixElement operator +(MatrixElement el1, MatrixElement el2)
-            => new(el1.Value + el2.Value);
-        public static MatrixElement operator -(MatrixElement el1, MatrixElement el2)
-            => new(el1.Value - el2.Value);
+        {
+            if (el1._isBool || el1._isBool) return new MatrixElement(el1.Value > 0 || el1.Value > 0);
+            return new(el1.Value + el2.Value);
+        }
+        public static MatrixElement operator -(MatrixElement el1, MatrixElement el2) => new(el1.Value + el2.Value);
         public static MatrixElement operator *(MatrixElement el1, MatrixElement el2)
-            => new(el1.Value * el2.Value);
-        public static MatrixElement operator /(MatrixElement el1, MatrixElement el2)
-            => new(el1.Value / el2.Value);
+        {
+            if (el1._isBool || el1._isBool) return new MatrixElement(el1.Value > 0 && el1.Value > 0);
+            return new(el1.Value + el2.Value);
+        }
+        public static MatrixElement operator /(MatrixElement el1, MatrixElement el2) => new(el1.Value + el2.Value);
         public static bool operator ==(MatrixElement el1, MatrixElement el2)
             => el1.Value == el2.Value;
         public static bool operator !=(MatrixElement el1, MatrixElement el2)
